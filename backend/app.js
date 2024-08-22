bdfe68f066ceb1453b7a1d5c448e0a97c24a259f
akshay56665 <156991241+akshay56665@users.noreply.github.com>
2024-08-22 11:43:03 +0530

    minor update

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const server=require("http").createServer(app);
 
 const io=require("socket.io")(server,{
     cors: {
-      origin: "http://localhost:5173"
+      origin: "https://meetroom-6tqw.onrender.com"
     }
 })
 
